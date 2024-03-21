3e4d083ddce4efdf8dda5e68b9303f4d1f60bace
Таня Дашипилова <tanya.volk01@mail.ru>
2024-03-21 13:21:22 +0800

    +setting js

---
+++ node.js
@@ -0,0 +1,15 @@
+// Стандарт кодирования airbnb, рекомендует
+// использовать, по возможности, одинарные
+console.log("Dracarys!");  
+
+// Экранируется только ", так как в этой ситуации
+// двойные кавычки имеют специальное значение
+console.log("Dragon's mother said \"No\"");
+// => Dragon's mother said "No" 
+
+// Обратный слеш `\` не выводится, если после него идет обычный символ,
+// а не специальный
+console.log("Death is \so terribly final, while life is full of possibilities");
+// => Death is so terribly final, while life is full of possibilities 
+
+console.log("\\ \\ \\\\ \\\ \'\"");
